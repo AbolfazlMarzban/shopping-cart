072132f5b67fab52cf71473b9d8cfeba979cbeea
AbolfazlMarzban <abolfazlmarzban39@gmail.com>
2023-08-03 20:19:09 +0330

    abol cart state

--- components/product.tsx
+++ components/product.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import {useRecoilState} from "recoil"
+import { cartState } from '@/atoms/cartState';
 
-function Product() {
+
+function Product({product}:any) {
+
+    const [cartItems, setCartItems] = useRecoilState(cartState)
+
     return (
         <div className='bg-white pt-6 shadow-2xl pb-4 rounded-2xl'>
-                <img className='mx-auto w-[350px] h-[200px] object-contain' src="/football.webp" alt="" />
+                <img className='mx-auto w-[350px] h-[200px] object-contain' src={product.image} alt="" />
                 <div className='mt-4 px-6'>
                     <div className='flex items-center justify-between text-[26px]'>
-                        <h1>footbal</h1>
-                        <h3>$399</h3>
+                        <h1>{product.name}</h1>
+                        <h3>${product.price}</h3>
                     </div>
-                    <button className='bg-red-600 text-white py-4 px-12 block mx-auto hover:bg-red-800'>
+                    <button className='bg-red-600 text-white py-4 px-12 block rounded-xl mx-auto hover:bg-red-800'>
                         Add to cart
                     </button>
                 </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import Navbar from "@/components/navbar"
 import Product from '@/components/product'
+import data from "../data.json"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,7 @@ export default function Home() {
     <section className='container mx-auto'>
       <h1 className='text-4xl mt-4 text-center'>Our Products</h1>
       <div className='mt-4 grid grid-cols-1 md:grid-cols-3 gap-4'>
-        <Product></Product>
+        {data.map(product => <Product product={product}></Product>)}
       </div>
     </section>
     </>
