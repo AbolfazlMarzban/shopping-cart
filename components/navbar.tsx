6f41292b6b7b53ee770dfb5b6a8cc1a65156449e
AbolfazlMarzban <abolfazlmarzban39@gmail.com>
2023-08-03 20:24:26 +0330

    abol cartstate

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,11 @@
+import { cartState } from "@/atoms/cartState";
 import Link from "next/link";
 import {FiShoppingCart} from "react-icons/fi"
+import {useRecoilState} from "recoil"
 
 function Navbar() {
+
+    const [cartItem] = useRecoilState(cartState)
     return (
         <section className="shadow-xl bg-white stocky top-0"> 
             <div className="container mx-auto py-2 text-[24px] flex justify-between	 items-center">  
@@ -12,7 +16,7 @@ function Navbar() {
                     <Link href={"/cart"}>
                         <div>
                             <FiShoppingCart></FiShoppingCart>
-                            <span className="absolute -top-2 -right-2 text-[13px] bg-red-600 h-[18px] w-[18px] rounded-full grid place-items-center text-white">0</span>
+                            <span className="absolute -top-2 -right-2 text-[13px] bg-red-600 h-[18px] w-[18px] rounded-full grid place-items-center text-white">{cartItem.length}</span>
                         </div>
                     </Link>
                 </div>
--- components/product.tsx
+++ components/product.tsx
@@ -7,6 +7,10 @@ function Product({product}:any) {
 
     const [cartItems, setCartItems] = useRecoilState(cartState)
 
+    const addItemstoCart = () => {
+        setCartItems((prevState) : any => [...prevState, product])
+    }
+
     return (
         <div className='bg-white pt-6 shadow-2xl pb-4 rounded-2xl'>
                 <img className='mx-auto w-[350px] h-[200px] object-contain' src={product.image} alt="" />
@@ -15,7 +19,7 @@ function Product({product}:any) {
                         <h1>{product.name}</h1>
                         <h3>${product.price}</h3>
                     </div>
-                    <button className='bg-red-600 text-white py-4 px-12 block rounded-xl mx-auto hover:bg-red-800'>
+                    <button className='bg-red-600 text-white py-4 px-12 block rounded-xl mx-auto hover:bg-red-800' onClick={addItemstoCart}>
                         Add to cart
                     </button>
                 </div>
