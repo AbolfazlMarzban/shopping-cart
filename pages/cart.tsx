2ff2d4af8c90076389875cef67b475754960ee54
AbolfazlMarzban <abolfazlmarzban39@gmail.com>
2023-08-04 11:14:04 +0330

    abol cart finish

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -6,7 +6,14 @@ import CartList from '@/components/CartList';
 
 function Cart() {
     const [cartItems, setCartItems] = useRecoilState(cartState)
+    const totalPrice = () => {
+        let total =0 
+        cartItems.forEach((item:any) => total += (item.price * item.quantity))
+        return total;
+    }
+    const createCheckoutSession = () => {
 
+    }
     return (
         <div>
             <Navbar></Navbar>
@@ -14,6 +21,11 @@ function Cart() {
                 {cartItems.length <= 0 ? <h1 className='text-center text-4xl mt-32'>Your Cart Is Empty!</h1> : 
                     cartItems.map((item:any) => <CartList key={item.id} data={item}></CartList>)
                 }
+                {cartItems.length > 0 && (<div className='max-w-[800px] mx-auto mt-4'>
+                    <h2 className='text-right text-3xl font-bold'>Total: ${totalPrice()}</h2>
+                    <button
+                        className='text-right bg-red-600 text-white py-4 px-12 mt-4 block mx-auto hover:bg-red-800 rounded-xl' onClick={createCheckoutSession}>Checkout</button>
+                </div>)}
             </div>
         </div>
     );
